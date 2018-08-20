b325c132d158e99ea07d178ad911eda0d642a8eb
Christina Zhang <christina10211@gmail.com>
2018-08-20 17:44:30 -0400

    update freezer file

--- app.py
+++ app.py
@@ -2,48 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
 from flask import render_template
-# app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schools.db'
-# db = SQLAlchemy(app)
-
-# class School(db.Model):
-#   __tablename__ = 'schools'
-#   __table_args__ = {
-#     'autoload': True,
-#     'autoload_with': db.engine
-#   }
-#   dbn = db.Column(db.String, primary_key=True)
-
-# @app.route("/")
-# def hello():
-#   schools = School.query.all()
-#   return render_template("list.html", schools=schools)
-
-
-# @app.route("/schools/")
-# def schools():
-#   schools = School.query.all()
-#   return render_template("list.html", schools=schools)
-
-
-# @app.route("/schools/<dbn>/")
-# def school(dbn):
-#   school = School.query.filter_by(dbn=dbn).first()
-#   return render_template("show.html", school=school)
-
-# # @app.route("/search")
-# # def search():
-# #   name = request.args.get("query")
-# #   schools = School.query.filter(School.school_name.contains("henry")).all()
-# #   return render_template("list.html", schools=schools)
-
-
-
-# # If this is running from the command line
-# # do something
-# if __name__ == '__main__':
-#   app.run(debug=True)
 
 
 app = Flask(__name__)
@@ -68,7 +27,7 @@ def markets():
   return render_template("list.html", markets = markets)
   
 
-@app.route("/markets/<FMID>")
+@app.route("/markets/<FMID>/")
 def market(FMID):
   market = Market.query.filter_by(FMID=FMID).first()
   return render_template("show.html", market=market)
